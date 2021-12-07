f78297b3daa80903af6dffbb6bd7e6d953195251
coloritoj <coloritoj@gmail.com>
2021-12-07 09:49:46 -0500

    Update

--- JoshDanTarikProject/Program.cs
+++ JoshDanTarikProject/Program.cs
@@ -8,94 +8,147 @@ namespace JoshDanTarikProject
         public string Title;
         public string Author;
         public string Status;
-        public double DueDate;
+        public DateTime DueDate;
 
-        public Book(string bookTitle, string bookAuthor, string bookStatus, double bookDueDate)
+        public Book(string bookTitle, string bookAuthor, string bookStatus)
         {
             Title = bookTitle;
             Author = bookAuthor;
             Status = bookStatus;
-            DueDate = bookDueDate;
         }
 
         public override string ToString()
         {
-            return $"\nTITLE: {Title} \nAUTHOR: {Author} \nSTATUS: {Status} \nDUEDATE: {DueDate}";
+            return $"\nTITLE: {Title} \nAUTHOR: {Author} \nSTATUS: {Status}";
         }
     }
 
     class Program
     {
+        static bool isValidCommand(string entry)
+        {
+            if (entry == "list books")
+            {
+                return true;
+            }
+            else if (entry == "search by author")
+            {
+                return true;
+            }
+            else if (entry == "search by title")
+            {
+                return true;
+            }
+            else if (entry == "check-out")
+            {
+                return true;
+            }
+            else if (entry == "return book")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
         static void Main(string[] args)
         {
+            DateTime testDateTime = DateTime.Now;
+
+            DateTime dueDateTime = testDateTime.AddDays(14);
+
             List<Book> testList = new List<Book>();
 
-            Book testbook = new Book("Dune", "Frank Herbert", "Checked-Out", 12.06);
+            Book testbook = new Book("Dune", "Frank Herbert", "Checked-Out");
             testList.Add(testbook);
-            testList.Add(new Book("Harry Potter & The Sorceror's Stone", "J.K. Rowling", "Checked-Out", 12.06));
-            testList.Add(new Book("To Kill a Mockingbird", "Harper Lee", "Checked-Out", 12.06));
-            testList.Add(new Book("1984", "George Orwell", "Checked-Out", 12.06));
-            testList.Add(new Book("The Great Gatsby", "F. Scott Fitzgerald", "Checked-Out", 12.06));
-            testList.Add(new Book("The Catcher and The Rye", "J.D. Salinger", "Checked-Out", 12.06));
-            testList.Add(new Book("Little Women", "Louisa May Alcott", "Checked-Out", 12.06));
-            testList.Add(new Book("Lord of the Flies", "William Golding", "Checked-Out", 12.06));
-            testList.Add(new Book("Fahrenheit 451", "Ray Bradbury", "Checked-Out", 12.06));
-            testList.Add(new Book("Jane Eyre", "Charlotte Bronte", "Checked-Out", 12.06));
-            testList.Add(new Book("Wuthering Heights", "Emily Bronte", "Checked-Out", 12.06));
-            testList.Add(new Book("Brave New World", "Aldous Huxley", "Available", 12.06));
+            testList.Add(new Book("Harry Potter & The Sorceror's Stone", "J.K. Rowling", "Checked-Out"));
+            testList.Add(new Book("To Kill a Mockingbird", "Harper Lee", "Checked-Out"));
+            testList.Add(new Book("1984", "George Orwell", "Checked-Out"));
+            testList.Add(new Book("The Great Gatsby", "F. Scott Fitzgerald", "Checked-Out"));
+            testList.Add(new Book("The Catcher and The Rye", "J.D. Salinger", "Checked-Out"));
+            testList.Add(new Book("Little Women", "Louisa May Alcott", "Checked-Out"));
+            testList.Add(new Book("Lord of the Flies", "William Golding", "Checked-Out"));
+            testList.Add(new Book("Fahrenheit 451", "Ray Bradbury", "Checked-Out"));
+            testList.Add(new Book("Jane Eyre", "Charlotte Bronte", "Checked-Out"));
+            testList.Add(new Book("Wuthering Heights", "Emily Bronte", "Checked-Out"));
+            testList.Add(new Book("Brave New World", "Aldous Huxley", "Available"));
 
-            Console.Write("Welcome to our library! What would you like to do? \n(LIST BOOKS/SEARCH BY AUTHOR/SEARCH BY TITLE KEYWORD/CHECK-OUT/RETURN BOOK): ");
-            string userEntry = Console.ReadLine();
-            bool goAgain = true;
-            int bookSelection = 0;
+            bool goAgain2 = true;
 
-            if (userEntry == "list books")
+            Console.WriteLine("Welcome to our library!");
+
+
+            do
             {
-                foreach (Book book in testList)
+                Console.Write("What would you like to do? \n(LIST BOOKS/SEARCH BY AUTHOR/SEARCH BY TITLE/CHECK-OUT/RETURN BOOK): ");
+                string userEntry = Console.ReadLine().ToLower();
+                bool goAgain = true;
+                int bookSelection = 0;
+
+                while (isValidCommand(userEntry) == false)
                 {
-                    Console.WriteLine(book);
+                    Console.Write("\nThat was not a valid command. What would you like to do? \n(LIST BOOKS/SEARCH BY AUTHOR/SEARCH BY TITLE/CHECK-OUT/RETURN BOOK): ");
+                    userEntry = Console.ReadLine().ToLower();
+                    isValidCommand(userEntry);
                 }
-            }
-            else if (userEntry == "search by author")
-            {
-                Console.Write("\nPlease enter the name of the author: ");
-                userEntry = Console.ReadLine();
 
-                foreach (Book book in testList)
+                if (userEntry == "list books")
                 {
-                    if (book.Author.Contains(userEntry))
+                    foreach (Book book in testList)
                     {
-                        Console.WriteLine(book.Title);
+                        Console.WriteLine(book);
                     }
                 }
-            }
-            else if (userEntry == "search by title")
-            {
-                Console.Write("\nPlease enter the name of the book: ");
-                userEntry = Console.ReadLine();
-
-                foreach(Book book in testList)
+                else if (userEntry == "search by author")
                 {
-                    if (book.Title.Contains(userEntry))
+                    Console.Write("\nPlease enter the name of the author: ");
+                    userEntry = Console.ReadLine();
+
+                    foreach (Book book in testList)
                     {
-                        Console.WriteLine(book.Title);
+                        if (book.Author.Contains(userEntry))
+                        {
+                            Console.WriteLine(book.Title);
+                        }
                     }
                 }
-            }
-            else if (userEntry == "check-out")
-            {
-                for (int i = 0; i < testList.Count; i++)
+                else if (userEntry == "search by title")
                 {
-                    Console.WriteLine($"{i + 1} - {testList[i].Title} - {testList[i].Author} - {testList[i].Status}");
-                }
+                    Console.Write("\nPlease enter the name of the book: ");
+                    userEntry = Console.ReadLine();
 
-                do
-                {
-                    Console.Write("\nWhich book would you like to check-out?: ");
-                    bookSelection = int.Parse(Console.ReadLine());
-                    if (testList[bookSelection - 1].Status == "Checked-Out")
+                    foreach (Book book in testList)
                     {
-                        Console.WriteLine("Sorry, that book is already checked-out.");
+                        if (book.Title.Contains(userEntry))
+                        {
+                            Console.WriteLine(book.Title);
+                        }
+                    }
+                }
+                else if (userEntry == "check-out")
+                {
+                    for (int i = 0; i < testList.Count; i++)
+                    {
+                        Console.WriteLine($"{i + 1} - {testList[i].Title} - {testList[i].Author} - {testList[i].Status}");
+                    }
+
+                    do
+                    {
+                        Console.Write("\nWhich book would you like to check-out?: ");
+                        bookSelection = int.Parse(Console.ReadLine());
+                        if (testList[bookSelection - 1].Status == "Checked-Out")
+                        {
+                            Console.WriteLine("Sorry, that book is already checked-out.");
+                        }
+                        else if (testList[bookSelection - 1].Status == "Available")
+                        {
+                            testList[bookSelection - 1].Status = "Checked-Out";
+                            testList[bookSelection - 1].DueDate = dueDateTime;
+                            Console.WriteLine($"Thanks for checking-out {testList[bookSelection - 1].Title}! It is due on {testList[bookSelection - 1].DueDate}.");
+                        }
 
                         Console.Write("\nWould you like to attempt to check-out another book? (yes/no): ");
                         userEntry = Console.ReadLine();
@@ -105,56 +158,48 @@ namespace JoshDanTarikProject
                             goAgain = false;
                         }
 
-                    }
-                    else if (testList[bookSelection - 1].Status == "Available")
-                    {
-                        testList[bookSelection - 1].Status = "Checked-Out";
-                        testList[bookSelection - 1].DueDate = 12.25;
-                        Console.WriteLine($"Thanks for checking-out {testList[bookSelection - 1].Title}! It is due on {testList[bookSelection - 1].DueDate}.");
-                    }
+                    } while (goAgain == true);
 
-                    Console.Write("\nWould you like to attempt to check-out another book? (yes/no): ");
-                    userEntry = Console.ReadLine();
-
-                    if (userEntry == "no")
-                    {
-                        goAgain = false;
-                    }
-
-                } while (goAgain == true);
-
-            }
-            else if (userEntry == "return book")
-            {
-                do
+                }
+                else if (userEntry == "return book")
                 {
-                    Console.WriteLine("Here are the books currently checked out:");
-
-                    for (int i = 0; i < testList.Count; i++)
+                    do
                     {
-                        if (testList[i].Status == "Checked-Out")
+                        Console.WriteLine("Here are the books currently checked out:");
+
+                        for (int i = 0; i < testList.Count; i++)
                         {
-                            Console.WriteLine($"{i + 1} - {testList[i].Title} - {testList[i].Author} - {testList[i].Status}");
+                            if (testList[i].Status == "Checked-Out")
+                            {
+                                Console.WriteLine($"{i + 1} - {testList[i].Title} - {testList[i].Author} - {testList[i].Status}");
+                            }
                         }
-                    }
 
-                    Console.Write("\nWhich book would you like to return? Please enter the corresponding number: ");
-                    bookSelection = int.Parse(Console.ReadLine());
+                        Console.Write("\nWhich book would you like to return? Please enter the corresponding number: ");
+                        bookSelection = int.Parse(Console.ReadLine());
 
-                    testList[bookSelection - 1].Status = "Available";
-                    Console.WriteLine($"Thank you for checking-in {testList[bookSelection - 1].Title}!");
+                        testList[bookSelection - 1].Status = "Available";
+                        Console.WriteLine($"Thank you for checking-in {testList[bookSelection - 1].Title}!");
 
-                    Console.Write("\nWould you like to return another book? (yes/no): ");
-                    userEntry = Console.ReadLine();
+                        Console.Write("\nWould you like to return another book? (yes/no): ");
+                        userEntry = Console.ReadLine();
 
-                    if (userEntry == "no")
-                    {
-                        goAgain = false;
-                    }
+                        if (userEntry == "no")
+                        {
+                            goAgain = false;
+                        }
 
-                } while (goAgain == true);
-               
-            } 
+                    } while (goAgain == true);
+                }
+                Console.Write("Would you like to go back to the menu? (yes/no): ");
+                userEntry = Console.ReadLine().ToLower();
+
+                if (userEntry == "no")
+                {
+                    goAgain2 = false;
+                }
+                Console.Clear();
+            } while (goAgain2 == true);
         }
     }
 }
